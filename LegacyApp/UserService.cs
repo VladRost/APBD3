43a7827b7218ca6c5ffa97af5661707849de44ce
VladRost <s28288@pjwstk.edu.pl>
2024-03-26 13:53:33 +0100

    interface clientrep

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -4,6 +4,12 @@ namespace LegacyApp
 {
     public class UserService
     {
+        private IClientRepository _clientRepository;
+
+        public UserService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
@@ -25,8 +31,8 @@ namespace LegacyApp
                 return false;
             }
 
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
+           
+            var client = _clientRepository.GetById(clientId);
 
             var user = new User
             {
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using LegacyApp;
+
 namespace TestProject1;
 
 public class UnitTest1
@@ -5,6 +7,18 @@ public class UnitTest1
     [Fact]
     public void AddUser_Should_Return_False_When_Email_Without_At_And_Dot()
     {
+        //Arrange
+        string firstName = "John";
+        string lastName = "Doe";
+        DateTime birthDate = new DateTime(1980, 1, 1);
+        int clientId = 1;
+        string email = "doe";
+        var service = new UserService();
         
+        //Act
+        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
+        
+        //Assert
+        Assert.Equal(false,result);
     }
 }
